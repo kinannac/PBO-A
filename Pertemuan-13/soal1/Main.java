d145417e31e89e3da00a39fd01a0bc257fe392b2
kinannac <kinantiayuc2405@gmail.com>
2025-11-11 19:23:46 +0700

    Implement Main class with cekMakhlukHidup method

---
+++ Pertemuan-13/soal1/Main.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main {
+    public void cekMakhlukHidup (MakhlukHidup mHidup){
+        mHidup.berdiri();
+        mHidup.oksigen();
+    }
+    
+    public static void main(String[] args) {
+        Main tMakhlukHidup = new Main();        
+        
+        tMakhlukHidup.cekMakhlukHidup(new Manusia("Dua Kaki"));       
+        
+        System.out.println("-----------------------------------------");                              
+        tMakhlukHidup.cekMakhlukHidup(new Hewan ("Empat Kaki","Dua Kaki"));
+        
+        System.out.println("-----------------------------------------");        
+        tMakhlukHidup.cekMakhlukHidup(new Tumbuhan ("Akar"));
+        
+    }
+}
