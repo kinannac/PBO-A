c1675639bd9c441f3271df6d3d7f8a08c672c4a2
kinannac <kinantiayuc2405@gmail.com>
2025-11-11 19:25:27 +0700

    Add Main class to run the Simulator
    Initial implementation of the Main class to run the simulator.

---
+++ Pertemuan-13/soal2/Main.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class SimulatorRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Simulator simulator = new Simulator(50, 50);
+        simulator.simulate(10);
+    }
+}
